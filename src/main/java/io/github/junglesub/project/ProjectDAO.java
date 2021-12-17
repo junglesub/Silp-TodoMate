3d8d87939e728c3fbd868e4d648493302616945d
junglesub <junglesubmarine@gmail.com>
2021-12-18 01:19:39 +0900

    Setup DB Model and get data

---
+++ src/main/java/io/github/junglesub/project/ProjectDAO.java
@@ -0,0 +1,34 @@
+package io.github.junglesub.project;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class ProjectDAO {
+	@Autowired
+	SqlSession sqlSession;
+	
+	public int createProject(ProjectVO vo) {
+		int result = sqlSession.insert("Project.createProject", vo);
+		return result;
+	}
+	
+	public ProjectVO getProject(String projectID) {
+		ProjectVO one = sqlSession.selectOne("Project.getProject", projectID);
+		return one;
+	}
+	
+//	public int deleteBoard(int seq) {
+//		int result = sqlSession.delete("Board.deleteBoard", seq);
+//		return result;
+//	}
+//	
+//	public int updateBoard(BoardVO vo) {
+//		int result = sqlSession.update("Board.updateBoard", vo);
+//		return result;
+//	}
+}
---
+++ src/main/java/io/github/junglesub/project/ProjectVO.java
@@ -0,0 +1,33 @@
+package io.github.junglesub.project;
+
+public class ProjectVO {
+	
+	private String projectId, title, owner;
+
+	public String getProjectId() {
+		return projectId;
+	}
+
+	public void setProjectId(String projectId) {
+		this.projectId = projectId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	
+	
+
+}
