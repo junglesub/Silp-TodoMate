16a85d1aa2f1f272dda506c1e93067a1dd721935
AnJW-HGU <22000404@handong.edu>
2021-12-18 18:33:15 +0900

    feat: Make creating new project

--- src/main/java/io/github/junglesub/controller/HomeController.java
+++ src/main/java/io/github/junglesub/controller/HomeController.java
@@ -6,18 +6,23 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import io.github.junglesub.board.BoardVO;
+import io.github.junglesub.project.ProjectVO;
+import io.github.junglesub.project.ProjectService;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
+	
+	@Autowired
+	ProjectService projectService;
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
@@ -39,8 +44,19 @@ public class HomeController {
 	}
 	
 	@RequestMapping(value = "/newproj", method = RequestMethod.GET)
-	public String createProject(Locale locale, Model model) {		
+	public String addProjectHome() {		
 		return "addprojectform";
 	}
 	
+	@RequestMapping(value = "/newproj/ok", method = RequestMethod.POST)
+	public String addProject(ProjectVO vo) {
+		String randomId = "";
+		for (int i=0; i<5; i++) {
+			randomId += (char) ((Math.random() * 26) + 97);
+		}
+		System.out.println(randomId);
+		vo.setProjectId(randomId);
+		projectService.createProject(vo);
+		return "redirect:/proj/"+vo.getProjectId();
+	}
 }
--- src/main/webapp/WEB-INF/views/addprojectform.jsp
+++ src/main/webapp/WEB-INF/views/addprojectform.jsp
@@ -46,10 +46,10 @@
 
 <div class="position-absolute top-50 start-50 translate-middle">
 	<h1 class="center-text">New project</h1>
-	<form action="" method="post">
+	<form action="./newproj/ok" method="post">
 		<div class="inputs">
-			<input class="form-control" id="name" placeholder="관리자 이름" autocomplete="off" />
-			<input class="form-control" id="project-code" placeholder="프로젝트 이름" />
+			<input class="form-control" id="owner" name="owner" placeholder="관리자 이름" autocomplete="off" />
+			<input class="form-control" id="title" name="title" placeholder="프로젝트 이름" />
 			<a href="./" role="button" class="btn btn-secondary">생성 취소</a>
 			<input type="submit" class="btn btn-primary" value="프로젝트 생성"/>
 		</div>
